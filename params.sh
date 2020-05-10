e25b8c4b228a322115447529e6c528361233d0c9
Kamol Mavlonov <kamoljan@gmail.com>
2020-05-10 09:15:48 +0900

    Add an ability to have common params

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 KEYS=`awk -F"=" '{print $1}' .env.sample`
-printf "\n        Environment:\n" > env.yaml
+#printf "\n        Environment:\n" > env.yaml
 for KEY in $KEYS
 do
   PARAM=`aws ssm get-parameters --name ${S3_APP_BUCKET_NAME}.${KEY} --with-decryption --query Parameters[0].Value`
