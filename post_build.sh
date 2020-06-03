fdcda6c0250b25f70919f2693d2736d8f8d5351f
Kamol Mavlonov <kamoljan@gmail.com>
2020-06-03 22:43:58 +0900

    Remove --no-include-email, it is deprecated

--- post_build.sh
+++ post_build.sh
@@ -6,7 +6,7 @@ if [ "$DEPLOY_ENVIRONMENT" = "development" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "staging" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "feature" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "hotfix" ]; then
-    $(aws ecr get-login --registry-ids "${AWS_ACCOUNT_ID}" --region $ECS_REGION --no-include-email)
+    $(aws ecr get-login --registry-ids "${AWS_ACCOUNT_ID}" --region $ECS_REGION)
     echo ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./docker.tag)
     docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./docker.tag)
 elif [ "$DEPLOY_ENVIRONMENT" = "production" ]; then
