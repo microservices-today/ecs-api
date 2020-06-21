a38885714d2b685b5246dd9d7fd69f9598f319d8
Kamol Mavlonov <kamoljan@gmail.com>
2020-06-21 10:18:48 +0900

    Make parameter store in hierarchy instead of root parameters

--- params.sh
+++ params.sh
@@ -3,7 +3,8 @@ KEYS=`awk -F"=" '{print $1}' .env.sample`
 #printf "\n        Environment:\n" > env.yaml
 for KEY in $KEYS
 do
-  PARAM=`aws ssm get-parameters --name ${S3_APP_BUCKET_NAME}.${KEY} --with-decryption --query Parameters[0].Value`
+  #PARAM=`aws ssm get-parameters --name ${S3_APP_BUCKET_NAME}/${KEY} --with-decryption --query Parameters[0].Value`
+  PARAM=`aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/${KEY} --with-decryption --query Parameters[0].Value`
   printf "        - Name: ${KEY}\n" >> env.yaml
   printf "          Value: ${PARAM}\n" >> env.yaml
 done
--- post_build.sh
+++ post_build.sh
@@ -12,10 +12,13 @@ if [ "$DEPLOY_ENVIRONMENT" = "development" ] || \
     docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./docker.tag)
 elif [ "$DEPLOY_ENVIRONMENT" = "production" ]; then
     # TODO: this may fail in production, please test it
-    $(aws ecr get-login --registry-ids "$SOURCE_AWS_ACCOUNT_ID" --region $SOURCE_AWS_REGION)
+    # $(aws ecr get-login --registry-ids "$SOURCE_AWS_ACCOUNT_ID" --region $SOURCE_AWS_REGION)
+    aws ecr get-login-password --region ${SOURCE_AWS_REGION} | docker login --username AWS --password-stdin ${SOURCE_AWS_ACCOUNT_ID}.dkr.ecr.${SOURCE_AWS_REGION}.amazonaws.com
     docker pull ${SOURCE_AWS_ACCOUNT_ID}.dkr.ecr.${SOURCE_AWS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./stage.tag)
     docker tag ${SOURCE_AWS_ACCOUNT_ID}.dkr.ecr.${SOURCE_AWS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./stage.tag) ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./prod.tag)
-    $(aws ecr get-login --registry-ids "${AWS_ACCOUNT_ID}" --region $ECS_REGION)
+    # TODO: Test in production
+    # $(aws ecr get-login --registry-ids "${AWS_ACCOUNT_ID}" --region $ECS_REGION)
+    aws ecr get-login-password --region ${ECS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com
     docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./prod.tag)
 else
     echo "NO POST BUILD ACTIONS IN RELEASE"
