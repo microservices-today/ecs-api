add627e8368b5346ea37d40eadfcfa67f7e13cd6
Kamol Mavlonov <kamoljan@gmail.com>
2020-06-21 10:32:05 +0900

    add missing quatation mark

--- params.sh
+++ params.sh
@@ -4,7 +4,7 @@ KEYS=`awk -F"=" '{print $1}' .env.sample`
 for KEY in $KEYS
 do
   #PARAM=`aws ssm get-parameters --name ${S3_APP_BUCKET_NAME}/${KEY} --with-decryption --query Parameters[0].Value`
-  PARAM=`aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/${KEY} --with-decryption --query Parameters[0].Value`
+  PARAM=`aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/${KEY}" --with-decryption --query Parameters[0].Value`
   printf "        - Name: ${KEY}\n" >> env.yaml
   printf "          Value: ${PARAM}\n" >> env.yaml
 done
