4028a0f1d772b467ea8e284d449a45655949d0b5
Kamol Mavlonov <kamoljan@gmail.com>
2020-07-11 23:29:07 +0900

    Chech python3 version

--- build.sh
+++ build.sh
@@ -32,7 +32,12 @@ elif [ "$DEPLOY_ENVIRONMENT" = "release" ] ; then
     git checkout master
     git merge staging
     git push origin master
+
+    python3 --version | true
+    sudo add-apt-repository universe
+    sudo apt-get update
     sudo apt-get install -y jq
+
     # Delete released branch (e.g. 2.0.4) and let a candidate (e.g. 2.0.4-candidate-f3056cc) to be promoted
     # This case is only for support of multiple pipelines
     #git push --delete origin ${RELEASE_PLAN} | true
