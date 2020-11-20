819635af49099cf1e157203da068914a75dd74be
Kamol Mavlonov <kamoljan@gmail.com>
2020-11-20 11:11:14 +0900

    Refactor code to use  notation instead of leagasy one

--- params.sh
+++ params.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-KEYS=`awk -F"=" '{print $1}' .env.sample`
+KEYS=$(awk -F"=" '{print $1}' .env.sample)
 for KEY in $KEYS
 do
-  PARAM=`aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/${KEY}" --with-decryption --query Parameters[0].Value`
+  PARAM=$(aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/${KEY}" --with-decryption --query Parameters[0].Value)
   printf "        - Name: ${KEY}\n" >> env.yaml
   printf "          Value: ${PARAM}\n" >> env.yaml
 done
