7be819a6a05874023ec616b5d04d8983951f7ab8
Kamol Mavlonov <kamoljan@gmail.com>
2021-01-10 18:29:46 +0900

    2.0.2 (#4)
    * Add a support for Fargate
    
    * convert task definition to support Fargate
    
    * Add ECSTaskExecutionRole for Fargate TaskDefinition
    
    * Fix Arn refferance issue
    
    * Set Cpu and Memory in Task level as Fargate requirement
    
    * Add X-Ray as sidecar for Fargate
    
    * fix spelling
    
    * remove HostPort
    
    * Fixate cpu and memory for x-ray task
    
    * Replace own xray image with official one
    
    * Share CPU resources
    
    * Share Memory amoung two task
    
    * Add MemoryReservation for xray task
    
    * Add MemoryReservation for main task
    
    * Add Memory in Task level
    
    * Temp fix for memory
    
    * Add policies to allow the ECS tasks to write to AWS X-Ray
    
    * Add TaskRoleArn to handle X-Ray restrictions
    
    * Remove AWS_XRAY_DAEMON_ADDRESS, it is unnecessary
    
    * Refactor code
    
    * Add TODO to not forget things
    
    * Fixate aws-xray-daemon version to v3.2.0
    
    * Remove CPU/Memory on container level
    
    * Inscrease default open file limit
    
    * Fix unsupported paramerter issue, looks like aws doc is outdated
    
    * Fix Ulimits issue
    
    * Move ulimits to ContainerDefinitions
    
    * Externalize ECSTaskExecutionRole to avoid recreation in this pipeline
    
    * Code refactoring
    
    * Add ListenerRule for the PublicLoadBalancer
    
    * Add path on the public LB that this service should be connected to
    
    * Add hosted base rules for public load balancer
    
    * Remove dependency on ALB
    
    * Add Service name into Target Group
    
    * Value of property Values must be of type List of String
    
    * Fix wrong formating
    
    * Fix YAML formating issue
    
    * Hardcode HOSTED_ZONE for testing purpose
    
    * Replace HOSTED_ZONE with DOMAIN_NAME
    
    * Have fun with sed command
    
    * Fix wrong parameter with sed command
    
    * Remove --no-include-email, it is deprecated
    
    * Add support for AWS CLI v2 for docker login
    
    * Add debuggin
    
    * Add ListenerPriority as param
    
    * Add ELB Listener Priority for a single ELB
    
    * Change ECS_LISTENER_PRIORITY to ELB_LISTENER_PRIORITY
    
    * Add /health for HealthCheckPath as a standard
    
    * Add a new line
    
    * Disable ELB Listener Priority for testing purpose
    
    * Set back ELB Listener Priority, it is required even with host based condition
    
    * Make configurable ELB Listener Priority from the service pipeline
    
    * Refactor code
    
    * Make parameter store in hierarchy instead of root parameters
    
    * add missing quatation mark
    
    * Fix DESIRED_COUNT bug
    
    * Fix github API Auth deprecation
    
    * fix formating in bash
    
    * Remove commentted code
    
    * Make Create a Release as Idenpotent
    
    * Remove sudo command
    
    * Use current time for docker image and git tag
    
    Use current EPOH for docker image and git tag instead of `CODEBUILD_BUILD_ID` environment variable.
    
    * Refactor code
    
    * Remove useless cut command
    
    * Fix sorting with git for latest tag
    
    * Add a tag for AWS resources via CloudFormation
    
    * Update readme with dependency between components
    
    * Update readme for readability
    
    * Remove outdated comments
    
    * Refactor code
    
    * Add a workaround for CF template body size with CF Stack
    
    * fix path to gateway.yaml
    
    * fix typo
    
    * fix template formating
    
    * Add Docker.com authentication to resolve API limit issue
    
    * Remove docker login from release pipeline
    
    * Move docker login on the top of the build.sh
    
    * Refactor code to use  notation instead of leagasy one
    
    * Refactor code to use  notation instead of leagasy one
    
    Co-authored-by: onekyat-pyaethuaung <34053706+onekyat-pyaethuaung@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ecs-api
 
-Holds ecs files for nodejs-app-pipeline
+This template provisions ECS service/task, ALB, API Gateway (optional), EFS (optional),
+Scaling Policy, Alarms, and Logging for your app.
+
+## Dependency between components
+
+[ecs-iac](https://github.com/microservices-today/ecs-iac) ->
+[ecs-cicd](https://github.com/microservices-today/ecs-cicd) ->
+[ecs-api](https://github.com/microservices-today/ecs-api) ->
+your app (e.g [ngp-nodejs](https://github.com/microservices-today/ngp-nodejs))
--- build.sh
+++ build.sh
@@ -2,20 +2,30 @@
 
 set -ex
 
-APP_NAME=`echo ${GITHUB_REPO} | sed 's/_/-/g'`
-APP_NAME=`echo ${APP_NAME} | sed 's/-//g'`
-APP_NAME=`echo ${APP_NAME} | cut -c1-15`
+APP_NAME=$(echo "${GITHUB_REPO}" | sed 's/_/-/g')
+APP_NAME=$(echo "${APP_NAME}" | sed 's/-//g')
+APP_NAME=$(echo "${APP_NAME}" | cut -c1-15)
 
-echo -n "$CODEBUILD_BUILD_ID" | sed "s/.*:\([[:xdigit:]]\{7\}\).*/\1/" > build.id
+if [ "$DEPLOY_ENVIRONMENT" != "release" ] ; then
+  # Docker.com authentication to solve API rate limit issue
+  # https://www.docker.com/increase-rate-limits?utm_source=docker&utm_medium=web%20referral&utm_campaign=pull%20limits%20home%20page&utm_budget=
+  DOCKERHUB_USER=$(aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/DOCKERHUB_USER" --with-decryption --query Parameters[0].Value --output text)
+  DOCKERHUB_TOKEN=$(aws ssm get-parameters --name "/${ENVIRONMENT_NAME}/DOCKERHUB_TOKEN" --with-decryption --query Parameters[0].Value --output text)
+  echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USER" --password-stdin
+fi
+
+# prints Epoch time e.g. 1595330840
+date +%s > build.id
+
 if [ "$DEPLOY_ENVIRONMENT" = "development" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "feature" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "hotfix" ]; then
-    echo -n "$TAG_NAME-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
-    docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$ECS_REGION.amazonaws.com/$ECR_NAME:$(cat docker.tag) .
+    echo "$TAG_NAME-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
+    docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$ECS_REGION.amazonaws.com/$ECR_NAME:"$(cat docker.tag)" .
     TAG=$(cat docker.tag)
 elif [ "$DEPLOY_ENVIRONMENT" = "staging" ] ; then
-    echo -n "${RELEASE_PLAN}-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
-    docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$ECS_REGION.amazonaws.com/$ECR_NAME:$(cat docker.tag) .
+    echo "${RELEASE_PLAN}-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
+    docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$ECS_REGION.amazonaws.com/$ECR_NAME:"$(cat docker.tag)" .
     TAG=$(cat docker.tag)
 elif [ "$DEPLOY_ENVIRONMENT" = "release" ] ; then
     GITHUB_TOKEN=${GITHUB_TOKEN}
@@ -24,36 +34,67 @@ elif [ "$DEPLOY_ENVIRONMENT" = "release" ] ; then
     git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}
     cd ${GITHUB_REPO}
     git checkout staging
-    git tag
     echo "$(git log `git describe --tags --abbrev=0`..HEAD --pretty=format:"<br>- %s%b<br>")" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>/g' | tr -d '\r' | sed "s/\"/'/g" > ./commits
     cat ./commits
-    git tag $(cat ../docker.tag)
+    git tag "$(cat ../docker.tag)"
     git push --tags
     git checkout master
     git merge staging
     git push origin master
+
+    # TODO(kamol): Remove it, this distro (ubuntu) is different from our default one (Amazon linux)
+    # https://github.com/microservices-today/ecs-cicd/blob/266abc5be50e8e6168186c7a01293b4aff36c315/pipeline.yaml#L391
+    # Image: "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    yum install -y jq
+
+    # Delete released branch (e.g. 2.0.4) and let a candidate (e.g. 2.0.4-candidate-f3056cc) to be promoted
+    # This case is only to support multiple pipelines
+    # Get a release by tag name (https://docs.github.com/en/rest/reference/repos#get-a-release-by-tag-nametags)
+    API_URI="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/tags/${RELEASE_PLAN}?access_token=${GITHUB_TOKEN}"
+    RELEASE_STATUS=$(curl --write-out %{http_code} --silent --output get_release_by_tag.txt "$API_URI")
+    if [ "${RELEASE_STATUS}" -eq 200 ]; then
+
+        # Delete a release (https://docs.github.com/en/rest/reference/repos#delete-a-release)
+        echo "Release found with status:${RELEASE_STATUS}. Deleting the release."
+        RELEASE_ID=$(cat get_release_by_tag.txt | jq -r '.id')
+        API_URI="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${RELEASE_ID}?access_token=${GITHUB_TOKEN}"
+        RELEASE_STATUS=$(curl --request DELETE --write-out %{http_code} --silent --output /dev/null "$API_URI")
+        if [ "${RELEASE_STATUS}" -ne 204 ]; then
+            echo "Failed to delete a release with status:${RELEASE_STATUS}."
+            exit 1;
+        else
+            echo "Release with tag ${RELEASE_PLAN} and ID ${RELEASE_ID} is deleted."
+        fi
+
+    else
+        echo "Release not found with tag ${RELEASE_PLAN} and status:${RELEASE_STATUS}."
+    fi
+
+    # Create a release (https://docs.github.com/en/rest/reference/repos#create-a-release)
+    echo "Creating a new release."
+    API_URI="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases"
     API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master",
     "name": "%s - (Release Notes)","body": "%s",
     "draft": false,"prerelease": false}' $RELEASE_PLAN $RELEASE_PLAN "$(cat commits)")
-    echo $API_JSON
-    API_URI="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases?access_token=${GITHUB_TOKEN}"
-    echo $API_URI
-    RELEASE_STATUS=$(curl --write-out %{http_code} --silent --output /dev/null --data "$API_JSON" "$API_URI")
-    if [ $RELEASE_STATUS != 201 ]; then
+    RELEASE_STATUS=$(curl -H 'Authorization: token '${GITHUB_TOKEN}'' --write-out %{http_code} --silent --output /dev/null --data "$API_JSON" "$API_URI")
+    if [ "${RELEASE_STATUS}" -ne 201 ]; then
         echo "Release Failed with status:${RELEASE_STATUS}"
         exit 1;
+    else
+        echo "Release creation is completed successfully"
     fi
+
     cd ..
 else
     echo "Entering Production Build"
     GITHUB_TOKEN=${GITHUB_TOKEN}
     git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}
-    cd ${GITHUB_REPO}
+    cd "${GITHUB_REPO}"
     git checkout staging
-    STAGE_TAG=$(git describe --tags --abbrev=0 --match "*candidate*")
-    TAG=$(curl https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest?access_token=${GITHUB_TOKEN} | grep tag_name | grep -Eo "([0-9]\.*)+")
-    echo $STAGE_TAG > ../stage.tag
-    echo $TAG > ../prod.tag
+    STAGE_TAG=$(git tag -l --sort=-v:refname '*candidate*' | head -n 1)
+    TAG=$(curl -H 'Authorization: token '${GITHUB_TOKEN}'' https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest | grep tag_name | grep -Eo "([0-9]\.*)+")
+    echo "${STAGE_TAG}" > ../stage.tag
+    echo "${TAG}" > ../prod.tag
     cat ../stage.tag
     cat ../prod.tag
     cd ..
@@ -75,20 +116,22 @@ if [ "$DEPLOY_ENVIRONMENT" != "release" ] ; then
 
   if [ "$S3_SWAGGER_BUCKET_NAME" != "" ] ; then
     sed -i "s@S3_SWAGGER_BUCKET_NAME@$S3_SWAGGER_BUCKET_NAME@g" ecs/service.yaml
+    sed -i "s@ECS_REGION@$ECS_REGION@g" ecs/service.yaml
     sed -i "s@GITHUB_REPO@$GITHUB_REPO@g" ecs/service.yaml
+    sed -i "s@GITHUB_REPO@$GITHUB_REPO@g" ecs/gateway.yaml
     sed -i "s@{{APP_NAME}}@$APP_NAME@g" swagger.yaml
     sed -i "s@{{DOMAIN_NAME}}@`echo $HOSTED_ZONE | sed s'/.$//'`@g" swagger.yaml
     sed -i "s@{{API_HOSTNAME}}@$API_HOSTNAME@g" swagger.yaml
-    cat swagger.yaml | sed 's/^/        /' > _swagger.yaml
-    perl -i -pe 's/OPEN_API_SPEC/`cat _swagger.yaml`/e' ecs/service.yaml
+    sed 's/^/        /' swagger.yaml > _swagger.yaml
+    perl -i -pe 's/OPEN_API_SPEC/`cat _swagger.yaml`/e' ecs/gateway.yaml
     aws s3 cp swagger.yaml s3://$S3_SWAGGER_BUCKET_NAME/$GITHUB_REPO.yaml
+    aws s3 cp ecs/gateway.yaml s3://$S3_SWAGGER_BUCKET_NAME/$GITHUB_REPO-gateway.yaml
   else
     sed -i "s@S3_SWAGGER_BUCKET_NAME@SKIP_API_GATEWAY@g" ecs/service.yaml
-    sed -i "s@OPEN_API_SPEC@        SKIP_API_GATEWAY@g" ecs/service.yaml
   fi
 
   . ecs/params.sh
   perl -i -pe 's/ENVIRONMENT_VARIABLES/`cat env.yaml`/e' ecs/service.yaml
   # Remove the env yaml (not to persist secrets)
-  rm env.yaml | true
+  rm env.yaml || true
 fi
